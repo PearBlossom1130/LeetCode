f25d7e2bab0a9c512c913b3c8d9ad6fe48babf18
Hwasoo Lee <Hwasoo.Lee@versanthealth.com>
2018-06-15 14:01:46 -0400

    [MASTER] Add 6 and 8

---
+++ 6.LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _6.LongestPalindromicSubstring
+{
+	using System.Text;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = Convert("LZAGHKTIOIJO", 3);
+		}
+
+		public static string Convert(string s, int row)
+		{
+			int len = s.Length;
+
+			var sb = new StringBuilder[row];
+
+			for (int j = 0; j < sb.Length; j++)
+			{
+				sb[j] = new StringBuilder();
+			}
+
+			int i = 0;
+			while (i < len)
+			{
+				for (int idx = 0; idx < row && i < len; idx++)
+				{
+					sb[idx].Append(s[i++]);
+				}
+
+				for (int idx = row-2; idx >= 1 && i < len; idx--)
+				{
+					sb[idx].Append(s[i++]);
+				}
+
+
+			}
+
+
+			for (int idx = 1; idx < sb.Length; idx++)
+			{
+				sb[0].Append(sb[idx]);
+			}
+
+			return sb[0].ToString();
+		}
+	}
+
+}
---
+++ 8.StringToInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _8.StringToInteger
+{
+	using System;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var d1 = "22222*";
+			var dd = d1.IndexOf("*");
+
+
+			//int d = Atoi("2147483648");
+            int d = Atoi("2047");
+        }
+
+		public static int Atoi(string s)
+		{
+			if (string.IsNullOrEmpty(s)) return 0;
+
+			int len = s.Length;
+			int j = 0;
+			int num = 1;
+			for (int i = 0; i < len; i++)
+			{
+				int v = s[i] - '0';
+				if (v > 9) return 0;
+				if (i != 0) num = num * 10 + v;
+				else num = v;
+				if (Int32.MaxValue < num || Int64.MinValue > num) return 0;
+			}
+
+			return num;
+		}
+	}
+}
